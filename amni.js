4c7cc44483251589914c3138fc8601c3630b8dfb
sydurubel <sydulrubel@gmail.com>
2021-01-22 04:44:26 +0600

    javascript

---
+++ amni.js
@@ -0,0 +1,36 @@
+// function hotelCost(days){
+//     var total = 0;
+//     if ( days <= 10){
+//         total = days * 100;
+//     }else if (days <= 20){
+//         var first = 10 * 100;
+//         var remain = days - 10;
+//         var second = remain * 80;
+//         total = first + second;
+//     }else {
+//         var first = 10 * 100;
+//         var second = 10 * 80;
+//         var remaining = days - 20;
+//         var third = remaining * 50;
+//         total = first + second + third;
+//     }
+//     return total;
+// }
+// var lagbe = hotelCost(25);
+// console.log(lagbe);
+
+
+// function megaFriend(frndList){
+//     var mFriend = " ";
+//     for ( var i = 0; i< frndList.length; i++){
+//         var megaName = frndList[i];
+//         if(megaName.length > mFriend.length ){
+//             mFriend = megaName;
+//         }
+//     }
+//     return mFriend;
+// }
+
+// friends = ['Joynal', 'Pitu', 'Jahid', 'Mamun', 'Sanjoy', 'Aminur', 'Shohel', 'Amranul', 'Nazrul', 'Jagannath']
+// var niceGuy = megaFriend(friends);
+// console.log(niceGuy);
---
+++ assignment.js
@@ -0,0 +1,52 @@
+// kilometerToMeter
+// function kilometerToMeter(km){
+//     var meter = km * 1000;
+//     return meter;
+// }
+// var dictance = kilometerToMeter(12);
+// console.log(dictance);
+
+
+
+
+
+
+
+
+
+// function hotelCost(days){
+//     var total = 0;
+//     if ( days <= 10){
+//         total = days * 100;
+//     }else if (days <= 20){
+//         var first = 10 * 100;
+//         var remain = days - 10;
+//         var second = remain * 80;
+//         total = first + second;
+//     }else {
+//         var first = 10 * 100;
+//         var second = 10 * 80;
+//         var remaining = days - 20;
+//         var third = remaining * 50;
+//         total = first + second + third;
+//     }
+//     return total;
+// }
+// var lagbe = hotelCost(32);
+// console.log(lagbe);
+
+
+// function megaFriend(frndList){
+//     var mFriend = " ";
+//     for ( var i = 0; i< frndList.length; i++){
+//         var megaName = frndList[i];
+//         if(megaName.length > mFriend.length ){
+//             mFriend = megaName;
+//         }
+//     }
+//     return mFriend;
+// }
+
+// friends = ['Joynal', 'Pitu', 'Jahid',  'Mamun', 'Sanjoy', 'Aminur', 'Shohel', 'Amranul', 'Nazrul', 'Jagannath']
+// var niceGuy = megaFriend(friends);
+// console.log(niceGuy);
